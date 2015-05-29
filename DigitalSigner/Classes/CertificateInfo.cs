4c99da76c6e736b11e3165d235226ca3e2e4b5d0
bibikov kirill <bibikov.k@list.ru>
2015-05-29 16:19:26 +0300

    second cert info

---
+++ DigitalSigner/Classes/CertificateInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSigner.Classes
+{
+    public class CertificateInfo
+    {
+        public string Name { get; set; }
+
+        public CertificateInfo()
+        {
+
+        }
+    }
+}
